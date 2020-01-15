9078520e52f06db4c72dbe3d6f42a192a4e48b49
jhuang <jhuang@fortinet.com>
2020-01-15 14:15:16 -0800

    test case for json library to support array

---
+++ test/array_test.cpp
@@ -0,0 +1,25 @@
+#include<nlohmann/json.hpp>
+#include<string>
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+	const string inFile{"action.txt"};
+	string l;
+
+	ifstream infile(inFile);
+	while(getline(infile, l)) {
+		replace(l.begin(), l.end(), '\'', '\"');
+		cout << "original json string:" << endl;
+		cout << l << endl;
+
+		nlohmann::json jobjs = nlohmann::json::parse(l);
+		cout << jobjs << endl ;
+		cout << "jobjs.size(): " <<jobjs.size() << endl;
+	}
+
+	return 0;
+	
+}
