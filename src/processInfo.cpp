376b1074722e591fa2e85b7520a3e8593aa66849
emilyhuang <emily.hby@gmail.com>
2020-01-15 01:21:40 -0500

    null

--- src/processInfo.cpp
+++ src/processInfo.cpp
@@ -14,16 +14,23 @@
 using namespace std;
 
 bool processInfo(vector<stock>& myportfolio, string jsonActStr, string jsonStoStr, string delimiter, double dividendIncome){
-    size_t position1 = 0, position2 = 0;
+    
     string actDate, actAction, actPrice, actTicker, actShares, stoActDate,stoActDividend, stoActSplit, stoActStock;
-    bool actFlag = false;
-    bool stoActFlag = false;
+    bool actReady = true;
+    bool stoActReady = true;
     cout << "string in action: " << jsonActStr << endl << endl;
     cout << "string in stock action: " << jsonStoStr << endl << endl;
-    while ((!actFlag && ((position1 = jsonActStr.find(delimiter)) != string::npos)) || 
-    (!stoActFlag && ((position2 = jsonStoStr.find(delimiter)) != string::npos))){
+
+    size_t position1 = jsonActStr.find(delimiter);
+    size_t position2 = jsonStoStr.find(delimiter);
+    while(actReady||stoActReady){
+    // cout << "this pos: " << pos2 << endl;
+    // while ((!actReady && ((position1 = jsonActStr.find(delimiter,position1+1)) != string::npos)) || 
+    // (!stoactReady && ((position2 = jsonStoStr.find(delimiter,position2+1)) != string::npos))){
+        cout << "pos1 " << position1 << endl;
+        cout << "pos2 " << position2 << endl; 
         if(position1 != string ::npos){
-            cout << "action position1:" << position1 << endl;
+            cout << "action position1: " << position1 << endl;
             string actionString = jsonActStr.substr(0, position1);
             actionString = actionString + "}";
             jsonActStr.erase(0, position1 + 1 + delimiter.length());
@@ -35,11 +42,16 @@ bool processInfo(vector<stock>& myportfolio, string jsonActStr, string jsonStoSt
             actPrice = inputAction["price"];
             actTicker = inputAction["ticker"];
             actShares = inputAction["shares"];
-            actFlag = false;
+            cout << actTicker << endl;
+            cout << actDate << endl;
+            cout << actPrice << endl;
+            cout << actShares << endl;
+            cout << actAction << endl;
+            actReady = false;
         }
 
         if(position2 != string::npos){
-            cout << "stock action position2:" << position2 << endl;
+            cout << "stock action position2: " << position2 << endl;
             string stoActString = jsonStoStr.substr(0, position2);
             stoActString = stoActString + "}";
             jsonStoStr.erase(0, position2 + 1 + delimiter.length());
@@ -50,7 +62,7 @@ bool processInfo(vector<stock>& myportfolio, string jsonActStr, string jsonStoSt
             stoActDividend = inputStockAction["dividend"];
             stoActSplit = inputStockAction["split"];
             stoActStock = inputStockAction["stocks"];
-            stoActFlag = false;
+            stoActReady = false;
         }
         
         if((position1 != string::npos)&&(position2 != string::npos)){
@@ -60,14 +72,14 @@ bool processInfo(vector<stock>& myportfolio, string jsonActStr, string jsonStoSt
                 cout << "On " << actDate << ", you have:" << endl;
                 bool update = updateAct(myportfolio, actAction, actTicker, actShares, actPrice);
                 printPorfolio(myportfolio, dividendIncome);
-                actFlag = true;
+                actReady = true;
             }else{
                 boost::replace_all(stoActDate, "/", "-");
                 string printDate = stoActDate.substr(0,10);
                 cout << "On " << stoActDate << ", you have:" << endl;
                 bool update = updateStoAct(myportfolio, stoActDividend, stoActSplit, stoActStock, dividendIncome);
                 printPorfolio(myportfolio, dividendIncome);
-                stoActFlag = true;
+                stoActReady = true;
             } 
         }else{
             if(position1 != string::npos && position2 == string::npos){
@@ -76,17 +88,24 @@ bool processInfo(vector<stock>& myportfolio, string jsonActStr, string jsonStoSt
                 cout << "On " << stoActDate << ", you have:" << endl;
                 bool update = updateStoAct(myportfolio, stoActDividend, stoActSplit, stoActStock, dividendIncome);
                 printPorfolio(myportfolio, dividendIncome);
-                stoActFlag = true;
+                stoActReady = true;
             }else if(position1 == string::npos && position1 == string::npos){
                 boost::replace_all(actDate, "/", "-");
                 string printDate = actDate.substr(0,10);
                 cout << "On " << actDate << ", you have:" << endl;
                 bool update = updateAct(myportfolio, actAction, actTicker, actShares, actPrice);
                 printPorfolio(myportfolio, dividendIncome);
-                actFlag = true;
+                actReady = true;
             }
-        }       
+        }
+        if(!actReady){
+            if(( position1 = jsonActStr.find(delimiter,position1+1)) != string::npos)actReady = true;
+        }   
+        if(!stoActReady){
+            if((position2 = jsonStoStr.find(delimiter,position2+1)) != string::npos)stoActReady = true;
+        }    
 	}
+
    // cout << '\t' << "- $" << fixed << setprecision(2) << dividendIncome << " of dividend income" << endl;
     return true;
 }
